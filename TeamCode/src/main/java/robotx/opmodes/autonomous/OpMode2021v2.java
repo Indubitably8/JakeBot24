d0958db6c9b2144e49d9b50d77272ee6d83c84c8
owenstuckman <73311727+owenstuckman@users.noreply.github.com>
2023-08-23 17:00:48 -0400

    initial cleaning of repo for 24 season

--- TeamCode/src/main/java/robotx/opmodes/autonomous/OpMode2021v2.java
+++ TeamCode/src/main/java/robotx/opmodes/autonomous/OpMode2021v2.java
@@ -3,8 +3,6 @@ package robotx.opmodes.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import robotx.libraries.XOpMode;
-import robotx.modules.ClawSystem;
-import robotx.modules.LiftMotors;
 import robotx.modules.OrientationDrive;
 
 @TeleOp(name = "OpMode 22-23", group = "Default")
@@ -12,21 +10,14 @@ import robotx.modules.OrientationDrive;
 public class OpMode2021v2 extends XOpMode {
 
     OrientationDrive orientationDrive;
-    ClawSystem clawSystem;
-    LiftMotors liftMotors;
 
     public void initModules() {
 
         super.initModules();
 
-        liftMotors = new LiftMotors(this);
-        activeModules.add(liftMotors);
-
         orientationDrive = new OrientationDrive(this);
         activeModules.add(orientationDrive);
 
-        clawSystem = new ClawSystem(this);
-        activeModules.add(clawSystem);
 
     }
 
