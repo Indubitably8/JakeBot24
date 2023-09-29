7723af99a7928aa4f158d3b4613f695f897a88cd
owenstuckman <73311727+owenstuckman@users.noreply.github.com>
2023-09-29 15:57:01 -0400

    Actually coded opmode
    Imported all modules into opmode, and I also coded grappling hook

---
+++ TeamCode/src/main/java/robotx/modules/GrapplingHook.java
@@ -0,0 +1,43 @@
+package robotx.modules;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import robotx.libraries.XModule;
+
+public class GrapplingHook extends XModule {
+
+
+    // motors being used
+
+    public DcMotor GrappleMotor;
+
+    double power = 1;
+
+    public GrapplingHook(OpMode op) {
+        super(op);
+    }
+
+    public void init() {
+
+        //init motors from configs
+
+        GrappleMotor = opMode.hardwareMap.dcMotor.get("GrappleMotor");
+
+    }
+
+    public void loop() {
+
+        if (xGamepad2().left_bumper.isDown()){
+            GrappleMotor.setPower(power);
+        }
+        else {
+            GrappleMotor.setPower(0.0);
+        }
+
+    }
+}
+
+/*
+x - intake
+ */
--- TeamCode/src/main/java/robotx/opmodes/autonomous/OpMode2021v2.java
+++ TeamCode/src/main/java/robotx/opmodes/autonomous/OpMode2021v2.java
@@ -3,13 +3,23 @@ package robotx.opmodes.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import robotx.libraries.XOpMode;
+import robotx.modules.GrapplingHook;
 import robotx.modules.OrientationDrive;
+import robotx.modules.ArmSystem;
+import robotx.modules.DroneLaunch;
+import robotx.modules.IntakeSystem;
+import robotx.modules.LiftMotors;
 
-@TeleOp(name = "OpMode 22-23", group = "Default")
+@TeleOp(name = "OpMode 23-24", group = "Default")
 
 public class OpMode2021v2 extends XOpMode {
 
     OrientationDrive orientationDrive;
+    ArmSystem armSystem;
+    DroneLaunch droneLaunch;
+    IntakeSystem intakeSystem;
+    LiftMotors liftMotors;
+    GrapplingHook grapplingHook;
 
     public void initModules() {
 
@@ -17,6 +27,21 @@ public class OpMode2021v2 extends XOpMode {
 
         orientationDrive = new OrientationDrive(this);
         activeModules.add(orientationDrive);
+
+        armSystem = new ArmSystem(this);
+        activeModules.add(armSystem);
+
+        droneLaunch = new DroneLaunch(this);
+        activeModules.add(droneLaunch);
+
+        intakeSystem = new IntakeSystem(this);
+        activeModules.add(intakeSystem);
+
+        liftMotors = new LiftMotors(this);
+        activeModules.add(liftMotors);
+
+        grapplingHook = new GrapplingHook(this);
+        activeModules.add(grapplingHook);
 
     }
 
