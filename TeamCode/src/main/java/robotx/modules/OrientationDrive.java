13f58b912aa34ac6d49e9dbdccf681f7d0751114
owenstuckman <73311727+owenstuckman@users.noreply.github.com>
2023-10-31 15:44:07 -0400

    orientation drive fix
    what is says

--- TeamCode/src/main/java/robotx/modules/OrientationDrive.java
+++ TeamCode/src/main/java/robotx/modules/OrientationDrive.java
@@ -47,13 +47,13 @@ public class OrientationDrive extends XModule {
 
     public void init(){
         frontLeft = opMode.hardwareMap.dcMotor.get("frontLeft");
-        //frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);  //when using dom drive train, have left reversed
         frontRight = opMode.hardwareMap.dcMotor.get("frontRight");
         //frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight = opMode.hardwareMap.dcMotor.get("backRight");
         //backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft = opMode.hardwareMap.dcMotor.get("backLeft");
-        //backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
