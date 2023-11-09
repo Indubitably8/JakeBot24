136731b8727d9b70a75fe9f618efc7f223b46197
owenstuckman <73311727+owenstuckman@users.noreply.github.com>
2023-11-09 11:50:37 -0500

    software team w
    software team doing stuff now after 1 week gap because we showing up again.

--- TeamCode/src/main/java/robotx/modules/DroneLaunch.java
+++ TeamCode/src/main/java/robotx/modules/DroneLaunch.java
@@ -9,19 +9,26 @@ public class DroneLaunch extends XModule {
 
     //var setup
     public Servo launchArm;
+    public Servo angleLaunch;
 
     boolean launched = false;
+    boolean angled = false;
 
-/*
 
     //methods are built into one button as a toggle
 
     public void launch() {
-            launchArm.setPosition(.36);
-            launched = true;
+        launchArm.setPosition(.36);
+        launched = true;
     }
 
-
+    public void angle() {
+        if (!angled) {
+            angleLaunch.setPosition(0);
+        } else {
+            angleLaunch.setPosition(0);
+        }
+    }
 
 
     public DroneLaunch(OpMode op) {
@@ -31,15 +38,19 @@ public class DroneLaunch extends XModule {
     public void init() {
         // pulls servos from configs
         launchArm = opMode.hardwareMap.servo.get("launchArm");
+        angleLaunch = opMode.hardwareMap.servo.get("angleLaunch");
     }
 
     public void loop() {
         // button presses, calls methods
 
-            if (xGamepad2().right_bumper.wasPressed()) {
-                if(!launched){
-                    launch();
-                }
+        if (xGamepad2().right_bumper.wasPressed()) {
+            if (!launched) {
+                launch();
             }
-    */
+        }
+        if (xGamepad2().left_trigger > 0.5) {
+            angle();
+        }
+    }
 }
