19862d7f7eaf3ea2ca8ac1a44084f1c12f95a4f5
owenstuckman <73311727+owenstuckman@users.noreply.github.com>
2023-12-05 16:49:32 -0500

    Place+Park auton
    BSL coded

--- TeamCode/src/main/java/robotx/opmodes/autonomous/ParkandPlace.java
+++ TeamCode/src/main/java/robotx/opmodes/autonomous/ParkandPlace.java
@@ -15,9 +15,9 @@ import robotx.modules.ArmSystem;
 import robotx.modules.IntakeSystem;
 import robotx.modules.LiftMotors;
 
-@Autonomous(name = "ParkEverywhere", group = "Default")
+@Autonomous(name = "ParkandPlace", group = "Default")
 
-public class ParkEverywhere extends LinearOpMode {
+public class ParkandPlace extends LinearOpMode {
 
     //private ElapsedTime runtime = new ElapsedTime();
 
@@ -25,6 +25,9 @@ public class ParkEverywhere extends LinearOpMode {
     MecanumDrive mecanumDrive;
     OrientationDrive orientationDrive;
     //OdomSystem odomSystem;
+    ArmSystem armSystem;
+    LiftMotors liftMotors;
+    IntakeSystem intakeSystem;
 
     @Override
 
@@ -43,9 +46,21 @@ public class ParkEverywhere extends LinearOpMode {
         //odomSystem = new OdomSystem(this);
         //odomSystem.init();
 
+        armSystem = new ArmSystem(this);
+        armSystem.init();
+
+        intakeSystem = new IntakeSystem(this);
+        intakeSystem.init();
+
+        liftMotors = new LiftMotors(this);
+        liftMotors.init();
+
         //odomSystem.start();
         mecanumDrive.start();
         orientationDrive.start();
+        armSystem.start();
+        intakeSystem.start();
+        liftMotors.start();
 
         mecanumDrive.frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         mecanumDrive.frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -142,9 +157,12 @@ public class ParkEverywhere extends LinearOpMode {
                     telemetry.addData("current run", sideSelect);
                     telemetry.update();
                     sleep(100);
-                    DriveForward(0.5,1700);
+                    StrafeRight(0.5,1200);
                     sleep(50);
-                    StrafeLeft(0.5,5000);
+                    ArmUp();
+                    DriveForward(0.5,2000);
+                    sleep(50);
+                    Release(500);
                     sleep(1000);
                     break;
             }
@@ -249,6 +267,67 @@ public class ParkEverywhere extends LinearOpMode {
         mecanumDrive.frontRight.setPower(0);
         mecanumDrive.backLeft.setPower(0);
         mecanumDrive.backRight.setPower(0);
+    }
+
+    public void Intake(double power, int time) {
+        intakeSystem.IntakeMotor.setPower(power);
+        sleep(time);
+        intakeSystem.IntakeMotor.setPower(0);
+    }
+
+    public void Unintake(double power, int time) {
+        intakeSystem.IntakeMotor.setPower(-power);
+        sleep(time);
+        intakeSystem.IntakeMotor.setPower(0);
+    }
+
+    public void FirstLift() {
+        double liftPower = 1;
+        int liftTime = 100;
+        liftMotors.LeftLift.setPower(liftPower);
+        liftMotors.RightLift.setPower(-liftPower);
+        sleep(liftTime);
+        liftMotors.LeftLift.setPower(0);
+        liftMotors.RightLift.setPower(0);
+    }
+
+    public void RaiseLift(double power, int time) {
+        liftMotors.LeftLift.setPower(power);
+        liftMotors.RightLift.setPower(-power);
+        sleep(time);
+        liftMotors.LeftLift.setPower(0);
+        liftMotors.RightLift.setPower(0);
+    }
+
+    public void LowerLift(double power, int time) {
+        liftMotors.LeftLift.setPower(-power);
+        liftMotors.RightLift.setPower(power);
+        sleep(time);
+        liftMotors.LeftLift.setPower(0);
+        liftMotors.RightLift.setPower(0);
+    }
+
+    public void ArmRest () {
+        armSystem.leftWrist.setPosition(.15);
+        armSystem.rightWrist.setPosition(.95);
+        armSystem.leftArm.setPosition(.29);
+        armSystem.rightArm.setPosition(.70);
+    }
+
+    public void ArmUp () {
+        armSystem.leftWrist.setPosition((.565));
+        armSystem.rightWrist.setPosition((.485));
+        sleep(500);
+        armSystem.leftWrist.setPosition(.98);
+        armSystem.rightWrist.setPosition(.02);
+        armSystem.leftArm.setPosition(.542);
+        armSystem.rightArm.setPosition(0.53);
+    }
+
+    public void Release(int time) {
+        armSystem.blockServo.setPosition(.6);
+        sleep(time);
+        armSystem.blockServo.setPosition(.1);
     }
 
 }
