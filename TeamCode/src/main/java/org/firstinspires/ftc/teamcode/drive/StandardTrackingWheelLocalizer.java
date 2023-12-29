31483f0da388ec26c3c22fbb22c7a0cd79042951
owenstuckman <73311727+owenstuckman@users.noreply.github.com>
2023-12-29 12:15:12 -0500

    constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -32,8 +32,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.96; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 12.4896; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 3.01; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 12.375; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = 4; // in; offset of the lateral wheel
 
     // put in these to tune dead wheels
     public static double X_MULTIPLIER = 1.017308371600141; // multiplies in x direction
